ab1de7224b567fbab46e6dba87381fb05a864bfc
Cambara <tiago.cambara21@gmail.com>
2016-08-21 23:28:53 -0300

    add footer

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -6,21 +6,25 @@
     <link rel="stylesheet" href="/assets/css/app.css">
 </head>
 <body>
-<header class="menu">
-    <div class="container">
-        <div class="header clearfix">
-            <h1 class="menu_title">Blog - Laravel</h1>
-            <nav class="menu_option">
-                <ul class="nav nav-pills">
-                    <li role="presentation"><a href="/">Home</a></li>
-                </ul>
-            </nav>
+    <header class="menu">
+        <div class="container">
+            <div class="header clearfix">
+                <h1 class="menu_title">Blog - Laravel</h1>
+                <nav class="menu_option">
+                    <ul class="nav nav-pills">
+                        <li role="presentation"><a href="/">Home</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
-    </div>
-</header>
+    </header>
 
     @yield('container')
-
+    <footer class="footer">
+        <div class="container">
+            <p>© Laravel-Blog 2016. All rights reserved.</p>
+        </div>
+    </footer>
 <!-- Scripts -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="/assets/js/bootstrap.js"></script>
