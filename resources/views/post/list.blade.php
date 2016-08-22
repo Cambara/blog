094d78bdb0fad2c32c7a024ca6daa5f80e5d54d2
Cambara <tiago.cambara21@gmail.com>
2016-08-21 23:12:22 -0300

    Add post list

--- resources/views/post/list.blade.php
+++ resources/views/post/list.blade.php
@@ -1,5 +1,16 @@
 @extends('templates.default')
 @section('title') Home @stop
 @section('container')
-    hi
+    <div class="container">
+        @foreach($posts as $post)
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-10 col-xs-12 post">
+                    <h3>{{ $post["title"] }}</h3>
+                    <div class="col-sm-12">
+                        <p>{{ $post["text"] }}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @stop
